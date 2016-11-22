29d17d7b5ecaa05036166cb74c1e01a8e58e38e5
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-11-21 22:19:44 -0200

    Fixed download call

--- companion.R
+++ companion.R
@@ -6,6 +6,6 @@ library(data.table)
 
 stormFileUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 destFileName <- "storm_data.csv.bz2"
-if (!file.exists(destFileName)) download(stormFileUrl, destFileName)
+if (!file.exists(destFileName)) download.file(stormFileUrl, destfile=destFileName)
 stormData <- read.csv("storm_data.csv.bz2")
 stormDataFrame <- select(stormData, EVTYPE, FATALITIES, INJURIES, PROPDMG, PROPDMGEXP, CROPDMG, CROPDMGEXP)
