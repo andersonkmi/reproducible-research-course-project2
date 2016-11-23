c644bd2681de789edc7d6716a21de46a612591b2
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-11-22 22:32:08 -0200

    More modifications

--- companion.R
+++ companion.R
@@ -6,5 +6,5 @@ library(ggplot2)
 stormFileUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 destFileName <- "storm_data.csv.bz2"
 if (!file.exists(destFileName)) download.file(stormFileUrl, destfile=destFileName)
-stormData <- read.csv("storm_data.csv.bz2")
+stormData <- read.csv("storm_data.csv.bz2", header=TRUE, sep=",")
 stormDataFrame <- select(stormData, EVTYPE, FATALITIES, INJURIES, PROPDMG, PROPDMGEXP, CROPDMG, CROPDMGEXP)
